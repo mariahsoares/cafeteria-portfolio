fbbed142933d82bc16998de374995dfe8ee12999
mariahsoares <msmariahxy@gmail.com>
2026-06-08 20:56:38 -0300

    feat: implement splash screen and video transition

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", () => {
+
+    const video = document.getElementById("video-cabecalho");
+
+    setTimeout(() => {
+
+        video.currentTime = 0;
+        video.play();
+
+    }, 3200);
+
+});
